d61480e5da094816f53054754857f30bd105aa05
Nadia Lutz <na.lutz@outlook.com>
2025-02-03 10:50:12 +0100

    had issues with the user variabel, so instead of - let iterator = user.values(); - I had to use - let iterator = Object.values(user); - otherwise I'd get a typeError

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ let user = {
     race: "Undead",
     spec: "Affliction",
     level: 54, 
-    guild: "Self Found" 
+    guild: "Self Found",
+    professions: ["Tailoring", "Enchanting", "First Aid"],
+    minion: {
+        type: "Succubus",
+        name: "Lyriel"
+    }
 }
-
 
 //opgave 1 
 
@@ -20,3 +24,13 @@ console.log(user["username"])
 console.log(user["race"])
 console.log(user["spec"])
 console.log(user["level"])
+
+//opgave 2 
+
+let iterator = Object.values(user);
+
+for (let value of iterator) {
+  console.log(value);
+}
+
+
