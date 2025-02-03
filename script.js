bafcc630366ae05f9a7895e8e1ac4040ed3fe560
Nadia Lutz <na.lutz@outlook.com>
2025-02-03 10:40:44 +0100

    created the different values of the array

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-let user {
-    username = "Vikka",
-    race = "Undead",
-    spec = "Affliction",
-    level = "54", 
-    guild = "Self Found" 
+
+let user = {
+    username: "Vikka",
+    race: "Undead",
+    spec: "Affliction",
+    level: 54, 
+    guild: "Self Found" 
 }
 
 
 //opgave 1 
+
+console.log(user.username)
+console.log(user.race)
+console.log(user.spec)
+console.log(user.level)
+
+
+console.log(user["username"])
+console.log(user["race"])
+console.log(user["spec"])
+console.log(user["level"])
